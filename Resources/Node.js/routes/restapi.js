275c015f0fd165534cf06beee2a069b0a071e6b0
Max <mnozin@gmail.com>
2015-07-08 17:59:30 -0400

    wip

---
+++ Resources/Node.js/routes/restapi.js
@@ -0,0 +1,29 @@
+
+var data = {
+  "posts": [
+    {
+      "title": "Lorem ipsum",
+      "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+      "title": "Sed egestas",
+      "text": "Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus."
+    }
+  ]
+};
+
+module.exports.allieml = function (req, res) {
+
+  //TODO add mingo call
+  var posts = [];
+  data.posts.forEach(function (post, i) {
+    posts.push({
+      id: i,
+      title: post.title,
+      text: post.text.substr(0, 50) + '...'
+    });
+  });
+  res.json({
+    posts: posts
+  });
+};
